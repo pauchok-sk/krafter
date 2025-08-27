5515728504bd5bf9fcdda1d62ae22eb25e5c9264
pauchok-sk <pauchoooooook@gmail.com>
2025-08-28 01:09:13 +0300

    s-steps dev

--- src/js/files/sliders.js
+++ src/js/files/sliders.js
@@ -4,9 +4,9 @@ export default function sliders() {
   if (introSlider) {
     const swiper = new Swiper(introSlider, {
       speed: 900,
-      // autoplay: {
-      //   delay: 4000,
-      // },
+      autoplay: {
+        delay: 4000,
+      },
       pagination: {
         el: ".intro__pagination-slider",
         clickable: true,
@@ -44,6 +44,35 @@ export default function sliders() {
           },
         },
       });
+    });
+  }
+
+  const projectsSlider = document.querySelector(".s-projects__slider");
+
+  if (projectsSlider) {
+    const swiper = new Swiper(projectsSlider, {
+      speed: 900,
+      slidesPerView: "auto",
+      spaceBetween: 8,
+      // autoplay: {
+      //   delay: 3200,
+      // },
+      initialSlide: 2,
+      centeredSlides: true,
+      navigation: {
+        nextEl: ".s-projects .slider-nav__btn._next",
+        prevEl: ".s-projects .slider-nav__btn._prev",
+      },
+      breakpoints: {
+        1200: {
+          slidesPerView: "auto",
+          spaceBetween: 40,
+        },
+        576: {
+          slidesPerView: "auto",
+          spaceBetween: 20,
+        },
+      },
     });
   }
 }
